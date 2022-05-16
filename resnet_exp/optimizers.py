a7bab4893fd8fde5ccd5e567ab4bd0a072532435
Gustavo Fuhr <gustavofuhr87@gmail.com>
2022-05-16 17:28:42 -0300

    ADD: timm for model, schedulers and optimizers

--- resnet_exp/optimizers.py
+++ resnet_exp/optimizers.py
@@ -7,7 +7,7 @@ def get_optimizer(model, optimizer_arg, weight_decay = 1e-4):
     if optimizer_arg == "adamp":
         optimizer = timm.optim.AdamP(model.parameters(), lr=0.01, weight_decay=weight_decay)
     elif optimizer_arg == "adam":
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=weight_decay)
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.1, weight_decay=weight_decay)
     elif optimizer_arg == "sgd":
         optimizer = torch.optim.SGD(model.parameters(), lr=0.01, weight_decay=weight_decay)
 
